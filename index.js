36f147b910e2ac649a6b0fc8528ce205709e2114
elif demirel <demirel.elif.35@gmail.com>
2020-04-13 00:47:59 +0300

    removed info

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 node_xj = require("xls-to-json");
   node_xj({
-    input: "shortfilms.xlsx",  // input xls
-    output: "shortfilms.json", // output json
-    sheet: "Sayfa1",  // specific sheetname
+    input: "input.xlsx",
+    output: "output.json",
+    sheet: "sheetname",
   }, function(err, result) {
     if(err) {
       console.error(err);
